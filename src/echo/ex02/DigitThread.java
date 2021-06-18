8d1f85646ab34338c61a129216e11fae24ceb514
wonho <wonho951@gmail.com>
2021-06-18 14:51:23 +0900

    2021.06.18 수업내용 (스레드)

--- src/echo/ex02/DigitThread.java
+++ src/echo/ex02/DigitThread.java
@@ -4,7 +4,7 @@ public class DigitThread extends Thread{	//	Thread는 만들어져 있응께 사
 
 	@Override
 	public void run() {	//부모(object)한테 있는 메소드
-		
+				//run 메소드 이름 바꾸면 안된다.
 		//0~9까지 출력하는거 만듦
 		for (int i = 0; i <= 9; i++) {
 			System.out.println(i);
