e95ac12fd3ff2876202ac2ec17ff55b18d6fade5
wonho <wonho951@gmail.com>
2021-06-18 15:37:35 +0900

    2021.06.18 스레드(숫자,대문자,소문자 출력하기)

--- src/echo/ex03/Multithread.java
+++ src/echo/ex03/Multithread.java
@@ -4,7 +4,7 @@ public class Multithread {
 
 	public static void main(String[] args) {
 		
-
+		//원래 메인에서는 시간순이지만 --> 각자 독립적으로 일하게 만듦.(멀티스레드)
 		
         Thread thread1 = new DigitThread();
         Thread thread2 = new LowerThread();
