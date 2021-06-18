a1698f9070b244735164c89073bee2b9e1830f69
wonho <wonho951@gmail.com>
2021-06-18 15:11:38 +0900

    2021.06.18 수업내용 (스레드)

--- src/echo/ex02/DigitThread.java
+++ src/echo/ex02/DigitThread.java
@@ -2,9 +2,19 @@ package echo.ex02;
 
 public class DigitThread extends Thread{	//	Thread는 만들어져 있응께 사용하면 됨.
 
+	
+	//필드
+	
+	//생성자
+	
+	//메소드 g/s
+	
+	//메소드-일반
+	
 	@Override
 	public void run() {	//부모(object)한테 있는 메소드
 				//run 메소드 이름 바꾸면 안된다.
+				//우리 로직 코드는 반드시 run()에 작성해야함.
 		//0~9까지 출력하는거 만듦
 		for (int i = 0; i <= 9; i++) {
 			System.out.println(i);
--- src/echo/ex02/MultithreadApp.java
+++ src/echo/ex02/MultithreadApp.java
@@ -4,6 +4,15 @@ public class MultithreadApp {
 
 	public static void main(String[] args) {
 		
+		
+		Thread thread = new DigitThread();	//섞어쓰기
+		thread.start();	//	start()는 내부적으로 run()을 호출하게 되어있음.
+						//	start() 내부에 출장준비(이동,자리세팅) --> 내부에서run()실행
+		
+		for (char ch = 'A'; ch <= 'Z'; ch++ ) {
+			System.out.println(ch);
+		}
+		
 
 	}
 
